420ce152b06cfd6aec645d0fd1261337142646d0
DesTroyerAb17 <arnesh10.b@gmail.com>
2025-03-14 00:00:25 +0530

    flutter push

---
+++ flutter_mediconnect/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mediconnect/firebase_options.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp( ProviderScope(child: MainApp()));
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Placeholder(),
+    );
+  }
+}
